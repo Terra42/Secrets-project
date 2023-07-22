07187b1d88626e61824d5dc9bdc7a490673fd381
Petra Zemanová <zemape42@gmail.com>
2023-07-22 17:27:24 +0200

    Secret project with Express and Body parser

---
+++ index.js
@@ -0,0 +1,40 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+const password = "ILoveProgramming";
+let validPassword = false;
+
+const evaluatePassword = (req, res, next) => {
+  req.body.password === password
+    ? (validPassword = true)
+    : (validPassword = false);
+  next();
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(evaluatePassword);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+  if (validPassword) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
